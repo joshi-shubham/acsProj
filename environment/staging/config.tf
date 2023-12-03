3509da13af0c39bb825cd50b4788d5abc77127a3
revati310 <revati.v.sharma83@gmail.com>
2023-12-03 16:28:51 -0500

    removed backup code

--- environment/staging/config.tf
+++
@@ -1,7 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = "final-project"      // Bucket where to SAVE Terraform State
-    key    = "staging-network/terraform.tfstate" // Object name in the bucket to SAVE Terraform State
-    region = "us-east-1"                     // Region where bucket is created
-  }
-}
