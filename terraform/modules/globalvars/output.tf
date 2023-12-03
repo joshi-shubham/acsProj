0c07f436b59f28dbaad8b9f771b29bd1ca087d9b
joshi-shubham <shubjoshi2056@gmail.com>
2023-12-03 14:50:00 -0500

    add the VPC module

---
+++ terraform/modules/globalvars/output.tf
@@ -0,0 +1,24 @@
+output "default_tags" {
+  value = {
+    "Owner" = "Enthusiasm"
+    "App" = "Web"
+  }
+}
+
+output "prefix" {
+  value = "Project"
+}
+
+output "instance_type" {
+  value = {
+    production = "t3.small",
+    staging = "t2.micro"
+  }
+}
+
+output "s3_dev_backend_bucket" {
+  value = "sjoshi73-project-backend"
+}
+output "s3_prod_backend_bucket" {
+  value = "acs730-assignment-sjoshi73"
+}
