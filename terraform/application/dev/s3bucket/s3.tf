d6b89e31e2411c362c890c63221a5d91b2cc9d30
joshi-shubham <shubjoshi2056@gmail.com>
2023-12-06 13:33:58 -0500

    added ignore in s3 tfsec

--- terraform/application/dev/s3bucket/s3.tf
+++ terraform/application/dev/s3bucket/s3.tf
@@ -14,7 +14,10 @@ data "terraform_remote_state" "network" {
     
   }
 }
-
+#tfsec:ignore:aws-s3-enable-bucket-encryption
+#tfsec:ignore:aws-s3-encryption-customer-key
+#tfsec:ignore:aws-s3-enable-versioning
+#tfsec:ignore:aws-s3-enable-bucket-logging
 resource "aws_s3_bucket" "web_s3_bucket" {
   bucket = "webimages-bucket"
 }
@@ -28,10 +31,13 @@ resource "aws_s3_bucket_ownership_controls" "web_s3_ownership_controls" {
 
 resource "aws_s3_bucket_public_access_block" "web_s3_public_access_block" {
   bucket = aws_s3_bucket.web_s3_bucket.id
-
+  #tfsec:ignore:aws-s3-block-public-acls
   block_public_acls       = false
+  #tfsec:ignore:aws-s3-block-public-policy
   block_public_policy     = false
+  #tfsec:ignore:aws-s3-ignore-public-acls
   ignore_public_acls      = false
+  #tfsec:ignore:aws-s3-no-public-buckets
   restrict_public_buckets = false
 }
 
