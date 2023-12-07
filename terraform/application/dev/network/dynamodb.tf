f6780e6332500a0259a821633b6a81212a780d2f
revati310 <revati.v.sharma83@gmail.com>
2023-12-06 23:37:12 -0500

    minor name changes

--- terraform/application/dev/network/dynamodb.tf
+++ terraform/application/dev/network/dynamodb.tf
@@ -1,9 +1,9 @@
 resource "aws_dynamodb_table" "dynamodb-terraform-state-lock" {
-  name = module.globalvars.state_lock_table
-  hash_key = "LockID"
-  read_capacity = 20
+  name           = module.globalvars.state_lock_table
+  hash_key       = "LockID"
+  read_capacity  = 20
   write_capacity = 20
- 
+
   attribute {
     name = "LockID"
     type = "S"
