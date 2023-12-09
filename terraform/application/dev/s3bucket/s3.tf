dcb93c7cbf2d51af72b6104b5be96af451a7af45
joshi-shubham <shubjoshi2056@gmail.com>
2023-12-09 14:53:11 -0500

    fixed the terrafrom lint issye

--- terraform/application/dev/s3bucket/s3.tf
+++ terraform/application/dev/s3bucket/s3.tf
@@ -8,7 +8,7 @@ provider "aws" {
 #tfsec:ignore:aws-s3-enable-versioning
 #tfsec:ignore:aws-s3-enable-bucket-logging
 resource "aws_s3_bucket" "web_s3_bucket" {
-  bucket = var.web-bucket
+  bucket        = var.web-bucket
   force_destroy = true
 }
 
@@ -22,11 +22,11 @@ resource "aws_s3_bucket_ownership_controls" "web_s3_ownership_controls" {
 resource "aws_s3_bucket_public_access_block" "web_s3_public_access_block" {
   bucket = aws_s3_bucket.web_s3_bucket.id
   #tfsec:ignore:aws-s3-block-public-acls
-  block_public_acls       = false
+  block_public_acls = false
   #tfsec:ignore:aws-s3-block-public-policy
-  block_public_policy     = false
+  block_public_policy = false
   #tfsec:ignore:aws-s3-ignore-public-acls
-  ignore_public_acls      = false
+  ignore_public_acls = false
   #tfsec:ignore:aws-s3-no-public-buckets
   restrict_public_buckets = false
 }
